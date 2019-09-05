54992188367d80e10dfc28a942fd4d15ae9e5f63
jacob <1anderson2jacob@gmail.com>
2019-09-04 18:00:16 -0700

    removed some comments from capitalize-names.test.js

--- week-1/wednesday/__tests__/capitalize-names.test.js
+++ week-1/wednesday/__tests__/capitalize-names.test.js
@@ -10,12 +10,3 @@ describe('normal inputs', () => {
     expect(capMe(['krisTopher', 'olIva', 'herminiA'])).toEqual(['Kristopher', 'Oliva', 'Herminia']);
   })
 })
-
-/*
-Test.assertSimilar(capMe(['samuel', 'MABELLE', 'letitia', 'meridith']), ['Samuel', 'Mabelle', 'Letitia', 'Meridith'])
-Test.assertSimilar(capMe(['Slyvia', 'Kristal', 'Sharilyn', 'Calista']), ['Slyvia', 'Kristal', 'Sharilyn', 'Calista'])
-Test.assertSimilar(capMe(['krisTopher', 'olIva', 'herminiA']), ['Kristopher', 'Oliva', 'Herminia'])
-Test.assertSimilar(capMe(['luke', 'marsha', 'stanford']), ['Luke', 'Marsha', 'Stanford'])
-Test.assertSimilar(capMe(['kara']), ['Kara'])
-Test.assertSimilar(capMe(['mARIANN', 'jOI', 'gEORGEANN']), ['Mariann', 'Joi', 'Georgeann'])
-*/
